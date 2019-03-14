02d5870b764f4057edcba2d18090312b8522526c
dss <vladislavsidl@gmail.com>
2019-03-14 20:11:29 +0300

    добавил многопоточность

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -8,7 +8,7 @@ public class Client {
         try (WWW www = new WWW("127.0.0.1", 8000)) {
             System.out.println("Connected to Server");
             String request = "Welcome";
-            System.out.println("Request: "+ request);
+            System.out.println("Request: " + request);
             www.writeLine(request);
             String response = www.readLine();
             System.out.println("Request:" + response);
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -8,22 +8,28 @@ public class Server {
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(8000)) {
 
-
             System.out.println("Server Started!");
-            while (true)
-                try (WWW www = new WWW(server)) {
+            while (true) {
+                WWW www = new WWW(server);
+                new Thread(() -> {
                     String request = www.readLine();
                     System.out.println(" Request: " + request);
-                    String response = (int)(Math.random()*30 -10) + " ";
+                    String response = (int) (Math.random() * 30 - 10) + " ";
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     www.writeLine(response);
-                    System.out.println("Response:"+ response);
-
-                } catch (NullPointerException e) {
-                    throw new RuntimeException(e);
-                }
+                    System.out.println("Response:" + response);
+                    try {
+                        www.close();
+                    } catch (IOException e) {
+                    }
+                }).start();
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
-
         }
     }
 }
